7ac0339517b346eb90ef3a83bb7d43ab031887e1
Rohit Chawhan <rohitchawhan555@gmail.com>
2021-08-06 16:14:26 +0530

    Changes For Deployment

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/register', {
+        fetch('https://mighty-wildwood-42011.herokuapp.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
